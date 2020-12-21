745b17538071093b52a2948c18c4782f7bc0f9e1
yamaguchi kaito <k.yamaguchi.reiwa00@gmail.com>
2020-12-21 13:56:08 +0900

    新しいbranchに入れる

---
+++ metal_slime_app/src/pages/todo/Form.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+
+const Form = ({addTodo}) => {
+const [value, setValue] = useState('')
+const handleSubmit = e => {
+  e.preventDefault()
+  console.log('value: ', value)
+
+  //JSを使ってフォーム内テキストをゲット
+  var reflesh = document.getElementById("inptext");
+
+  if(reflesh.value === "")
+    alert("中身が空です");
+  else
+    addTodo(value)
+
+  //JSを使ってフォーム内テキストをリセット
+    reflesh.value ="";
+}
+
+  return (
+    <form onSubmit = {handleSubmit}>
+      <input
+        type="text"
+        id = "inptext"
+        onChange={e => {
+          setValue(e.target.value)
+        }}
+      />
+      <input type="submit" />
+    </form>
+  )
+}
+
+export default Form
