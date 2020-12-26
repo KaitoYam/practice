101db7a76dabd69d0627ed2c4f1aae6a5ca3c74a
jack2020457 <k.chanmpmp@icloud.com>
2020-12-26 23:10:29 +0900

    12/26 プロフィール画面未完成／アップロード機能のみ

--- metal_slime_app/src/profile/UpDate.js
+++ metal_slime_app/src/profile/UpDate.js
@@ -1,24 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { Link } from 'react-router-dom'
+
 import firebase, { storage } from '../config/Firebase'
+import { AuthContext } from '../AuthService'
 
 const UpDate = () => {
     const [image, setImage] = useState('')
     const [imageUrl, setImageUrl] = useState('')
     const handleImage = e => {
         const image = e.target.files[0];
+        // e.target.files[0]　でファイル選択で選ばせたファイルになる
         setImage(image)
     }
+    
+    const user = useContext(AuthContext)
 
+    // const updateProfileDate = {
+    //     imageUrl:''
+    // }
+    const handleUpdate = e => {
+        e.preventDefault()
+        // if (imageUrl !== user.imageUrl) {
+        //     updateProfileDate = {
+        //         ...updateProfileDate,imageUrl:imageUrl
+        //     }
+        // }
+    }
     const onSubmit = e => {
         e.preventDefault()
         if (image === '') {
-            alert('ファイルが選択されていません')
+            return;
         }
-        // アップロード処理
 
+        
+        
+        firebase.firestore().collection('profile').add({
+            user: user.displayName,
+            date: new Date(),
+            imgUrl:imageUrl
+        })
+        
+        // アップロード処理
+        
         const uploadTask = storage.ref(`/images/${image.name}`).put(image);
+        // storage.ref(`/images/${image.name}`) でfirebase storageにフォルダとファイル名を指定し、アップロード
         uploadTask.on(
-            firebase.storage.TaskEvent.STAET_CAHNGED,
+            firebase.storage.TaskEvent.state_changed,
             next,
             error,
             complete
@@ -26,6 +53,7 @@ const UpDate = () => {
         // nextはアップロードの進行度や状態を取得するための関数
         // error　エラーが起きた時の処理
         // complete アップロード成功後の処理
+
     }
 
     const next = snapshot => {
@@ -38,7 +66,7 @@ const UpDate = () => {
 
     const error = () => {
         // エラーハンドリング
-        alert('エラー')
+        console.log('error')
     }
 
     const complete = () => {
@@ -51,12 +79,20 @@ const UpDate = () => {
 
     return (
         <div>
-            <h1>画像アップロード</h1>
+            <h1>プロフィール編集画面</h1>
+            <button onClick={() => firebase.auth().signOut()}>Logout</button>
+            <div>
+                <p><Link to="/room" >トーク</Link></p>
+                <p><Link to="/todo" >Todo</Link></p>
+                <p><Link to="/Recommended" >おすすめ</Link></p>
+                <p><Link to="/album" >卒業アルバム</Link></p>
+            </div>
             <form onSubmit={onSubmit}>
                 <input type='file' onChange={handleImage} />
                 <button>Upload</button>
             </form>
-            <img src={imageUrl} alt='uploaded' />
+            <img src={imageUrl} alt='' />
+            {/* <button　onSubmit={handleUpdate}>更新</button> */}
         </div>
     )
 }
