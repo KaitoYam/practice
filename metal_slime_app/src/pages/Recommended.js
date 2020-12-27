205af686bd9564e456b42d2c2e0a75772ba7c04e
yamaguchi kaito <k.yamaguchi.reiwa00@gmail.com>
2020-12-27 16:32:11 +0900

    おすすめの一覧の表示にマテリアルデザインを導入

--- metal_slime_app/src/pages/Recommended.js
+++ metal_slime_app/src/pages/Recommended.js
@@ -1,16 +1,57 @@
 import React from 'react'
 import css from './style.css'
-
 import { Link } from 'react-router-dom'
-
 import firebase from '../config/Firebase'
 import Button from '@material-ui/core/Button';
-
-
-
-const Recommended = () => {
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
     return (
-
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        // backgroundColor: theme.palette.background.paper,
+    },
+}));
+const Recommended = () => {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    return (
         <div className={css}>
             <h1 className="title_osusume">おすすめ</h1>
             <Button size="small" variant="outlined" onClick={() => firebase.auth().signOut()}>ログアウト</Button>
@@ -19,22 +60,47 @@ const Recommended = () => {
                 <p><Link to="/todo" className="link_osusume">Todo</Link></p>
                 <p className="nav_osusume">おすすめ</p>
                 <p><Link to="/album" className="link_osusume">卒業アルバム</Link></p>
-                
             </div>
-
-            <input type="radio" name="tab_item" id="tab_radio_A" className="tab_radio" defaultChecked />
+            <Paper className={classes.root}>
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" centered>
+                    <Tab label="山口海斗" {...a11yProps(0)} />
+                    <Tab label="鈴木孝太郎" {...a11yProps(1)} />
+                    <Tab label="松本彪" {...a11yProps(2)} />
+                </Tabs>
+            </Paper>
+            {/* <input type="radio" name="tab_item" id="tab_radio_A" className="tab_radio" defaultChecked />
             <label htmlFor="tab_radio_A" className="tab_menu">山口海斗</label>
-
-
             <input type="radio" name="tab_item" id="tab_radio_B" className="tab_radio" />
             <label htmlFor="tab_radio_B" className="tab_menu">鈴木孝太郎</label>
-
             <input type="radio" name="tab_item" id="tab_radio_C" className="tab_radio" />
-            <label htmlFor="tab_radio_C" className="tab_menu">松本彪</label>
-
-
-
-            <div id="tab_contains_A" className="tab_contains">
+            <label htmlFor="tab_radio_C" className="tab_menu">松本彪</label> */}
+            <TabPanel value={value} index={0}>
+                <ul>
+                    <li>言語に限らず、firestoreやwordpressなど幅広移動ががある。動画も一つ一つが軽いので取り組みやすいです！</li>
+                    <a href="https://dotinstall.com/">ドットインストール</a>
+                    <li>gitのコマンドについて漫画で教えてくれます。意外とわかりやすい。</li>
+                    <a href="https://www.r-staffing.co.jp/engineer/entry/20190621_1">gitを漫画で理解</a>
+                </ul>
+      </TabPanel>
+            <TabPanel value={value} index={1}>
+                <ul>
+                    <li>gitコマンドの意味がまとまっていて、分からない時に役立ちます！</li>
+                    <a href="https://kitsune.blog/git-command">gitコマンドの使い方 まとめ集</a>
+                    <li>VS codeのショートカットキーで作業効率が上がるので便利です！</li>
+                    <div id="iframe-parent">
+                        <iframe width="1280" height="720" src="https://www.youtube.com/embed/cNyvefTJOcM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>【完全保存版】VS Code ショートカット</iframe>
+                    </div>
+                </ul>
+      </TabPanel>
+            <TabPanel value={value} index={2}>
+                <ul>
+                    <li>初心者でもわかりやすいコードの基本的な書き方が学べます。</li>
+                    <a href="https://prog-8.com/">Progate</a>
+                    <li>gitがわからなくなったときにこれを見れば大体解決できます。</li>
+                    <a href="https://backlog.com/ja/git-tutorial/">サル先生のGit入門</a>
+                </ul>
+      </TabPanel>
+            {/* <div id="tab_contains_A" className="tab_contains">
                 <ul>
                     <li>言語に限らず、firestoreやwordpressなど幅広移動ががある。動画も一つ一つが軽いので取り組みやすいです！</li>
                     <a href="https://dotinstall.com/">ドットインストール</a>
@@ -42,8 +108,6 @@ const Recommended = () => {
                     <a href="https://www.r-staffing.co.jp/engineer/entry/20190621_1">gitを漫画で理解</a>
                 </ul>
             </div>
-
-
             <div id="tab_contains_B" className="tab_contains">
                <ul>
                    <li>gitコマンドの意味がまとまっていて、分からない時に役立ちます！</li>
@@ -54,20 +118,15 @@ const Recommended = () => {
                    </div>
                </ul>
             </div>
-
-
             <div id="tab_contains_C" className="tab_contains">
                 <ul>
                     <li>初心者でもわかりやすいコードの基本的な書き方が学べます。</li>
                     <a href="https://prog-8.com/">Progate</a>
                     <li>gitがわからなくなったときにこれを見れば大体解決できます。</li>
                     <a href="https://backlog.com/ja/git-tutorial/">サル先生のGit入門</a>
-
-
                 </ul>
-            </div>
+            </div> */}
         </div>
     )
-
 }
 export default Recommended
--- metal_slime_app/src/pages/album.js
+++ metal_slime_app/src/pages/album.js
@@ -9,7 +9,7 @@ const album = () => {
     return (
         <div className={css}>
             <h1 className="title_album">卒業アルバム</h1>
-            <Button size="small" variant="outlined"　onClick={() => firebase.auth().signOut()}>ログアウト</Button>
+            <Button size="small" variant="outlined" onClick={() => firebase.auth().signOut()}>ログアウト</Button>
             <div className="wrap_nav_album">
                 <p><Link to="/Room" className="link_album">トーク</Link></p>
                 <p><Link to="/todo" className="link_album">Todo</Link></p>
@@ -18,26 +18,26 @@ const album = () => {
             </div>
                 <div className="table-scroll"> 
 
-                    <table border="1">
+                <table border="1" border-collapse="collapse">
                         <tr>
-                            <th>名前</th>
-                            <th>目標</th>
-                            <th>全体の授業を通しての感想</th>
+                            <th bgcolor="skyblue">名前</th>
+                            <th bgcolor="skyblue">目標</th>
+                            <th bgcolor="skyblue">全体の授業を通しての感想</th>
                         </tr>
                         <tr>
-                            <td bgcolor="skyblue">山口 海斗</td>
-                            <td bgcolor="skyblue">ひとまず自社開発に勤める（サイバーエージェント入りたい。。。）</td>
-                            <td bgcolor="skyblue">講義の内容だけにとどまらず、いろいろなことを教えていただきとても刺激的で楽しかったです。</td>
+                            <td bgcolor="white">山口 海斗</td>
+                            <td bgcolor="white">ひとまず自社開発に勤める（サイバーエージェント入りたい。。。）</td>
+                            <td bgcolor="white">講義の内容だけにとどまらず、いろいろなことを教えていただきとても刺激的で楽しかったです。</td>
                         </tr>
                         <tr>
-                            <td bgcolor="orange">鈴木 孝太郎</td>
-                            <td bgcolor="orange">エンジニア転職を来年の3月までに目指して頑張ります！</td>
-                            <td bgcolor="orange">HTMLからJavaScript、Reactと幅広い言語とチーム開発の基礎などを学べて、すごく貴重な3ヶ月でした。これを踏まえて、より知識を深めてアプリケーションを作りたいと思いました。</td>
+                            <td bgcolor="white">鈴木 孝太郎</td>
+                            <td bgcolor="white">エンジニア転職を来年の3月までに目指して頑張ります！</td>
+                            <td bgcolor="white">HTMLからJavaScript、Reactと幅広い言語とチーム開発の基礎などを学べて、すごく貴重な3ヶ月でした。これを踏まえて、より知識を深めてアプリケーションを作りたいと思いました。</td>
                         </tr>
                         <tr>
-                            <td bgcolor="yellowgreen">松本 彪</td>
-                            <td bgcolor="yellowgreen">ベンチャー企業の会社でエンジニア就職</td>
-                            <td bgcolor="yellowgreen">基礎の部分から発展した知識まで幅広く学習すことができたのでいい経験になりました。チームでの開発は自分自身に責任感ありましたが多くのことを学べて楽しかったです。</td>
+                            <td bgcolor="white">松本 彪</td>
+                            <td bgcolor="white">ベンチャー企業の会社でエンジニア就職</td>
+                            <td bgcolor="white">基礎の部分から発展した知識まで幅広く学習すことができたのでいい経験になりました。チームでの開発は自分自身に責任感ありましたが多くのことを学べて楽しかったです。</td>
                         </tr>
                     </table>
                 </div>
