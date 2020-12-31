c5f9c064e1648f6781a55b56b1e0f93403c883ca
jack2020457 <k.chanmpmp@icloud.com>
2020-12-31 11:19:17 +0900

    12/31 profile,room 再編集

--- metal_slime_app/src/pages/Room.js
+++ metal_slime_app/src/pages/Room.js
@@ -7,9 +7,7 @@ import { Link } from 'react-router-dom'
 
 import './room.css'
 import profile from '../img/profile.img.jpg'
-import Button from '@material-ui/core/Button'
-
-import Button from '@material-ui/core/Button';
+import { Button, Paper } from '@material-ui/core'
 
 const Room = () => {
 
@@ -32,79 +30,81 @@ const Room = () => {
                 setImage(fireBaseUrl)
             })
         }
-}, [user])
+    }, [user])
 
 
-const handleSubmit = e => {
-    e.preventDefault()
-    const textNone = document.getElementById('tuika')
-    if (textNone.value === '')
-        return;
-    else
-        textNone.value = ''
-    firebase.firestore().collection('messages')
-        .add({
-            user: user.displayName,
-            content: value,
-            image: image,
-            date: new Date()
-        })
-    setMessages([
-        ...messages,
-        {
-            user: user.displayName,
-            image: image,
-            content: value,
-            date: new Date()
-        }
-    ])
-}
+    const handleSubmit = e => {
+        e.preventDefault()
+        const textNone = document.getElementById('tuika')
+        if (textNone.value === '')
+            return;
+        else
+            textNone.value = ''
+        firebase.firestore().collection('messages')
+            .add({
+                user: user.displayName,
+                content: value,
+                image: image,
+                date: new Date()
+            })
+        setMessages([
+            ...messages,
+            {
+                user: user.displayName,
+                image: image,
+                content: value,
+                date: new Date()
+            }
+        ])
+    }
 
 
-return (
-    <div>
-        <h1 className="title_chat">トーク</h1>
-        <div className="goto_update">
-            <Button size='small' variant='outlined' onClick={() => firebase.auth().signOut()}>ログアウト</Button>
-            <p><Link to="/update" className="link_chat">プロフィール変更</Link></p>
+    return (
+        <div>
+            <h1 className="title_chat">トーク</h1>
+            <div className="goto_update">
+                <Button size='small' variant='outlined' onClick={() => firebase.auth().signOut()}>ログアウト</Button>
+                <p><Link to="/update" className="link_chat">プロフィール変更</Link></p>
+            </div>
+            <div className="wrap_nav_chat">
+                <p className="nav_chat">トーク</p>
+                <p><Link to="/todo" className="link_chat">Todo</Link></p>
+                <p><Link to="/Recommended" className="link_chat">おすすめ</Link></p>
+                <p><Link to="/album" className="link_chat">卒業アルバム</Link></p>
+            </div>
+            <div className='took'>
+                <Paper>
+                    <ul className='room-ul'>
+                        {messages ?
+                            messages.map((message, id) =>
+                            (<li key={id} className='messages' >
+                                <div className='icon'>
+                                    <img className='icon-img' src={message.image ? message.image : profile} />
+                                </div>
+                                <div>
+                                    <p >{message.user}</p>
+                                    <p className='message'>{message.content}</p>
+                                </div>
+                            </li>)
+                            ) :
+                            <p>...loading</p>
+                        }
+                    </ul>
+                </Paper>
+                <form onSubmit={handleSubmit} className='took-form'>
+                    <div className='took-form2'>
+                        <input
+                            type='text'
+                            id='tuika'
+                            placeholder='メッセージを入力'
+                            onChange={e => setValue(e.target.value)}
+                        />
+                        <div type='submit' className='took-button'></div>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div className="wrap_nav_chat">
-            <p className="nav_chat">トーク</p>
-            <p><Link to="/todo" className="link_chat">Todo</Link></p>
-            <p><Link to="/Recommended" className="link_chat">おすすめ</Link></p>
-            <p><Link to="/album" className="link_chat">卒業アルバム</Link></p>
-        </div>
-        <div className='took'>
-            <ul className='room-ul'>
-                {messages ?
-                    messages.map((message, id) =>
-                    (<li key={id} className='messages' >
-                        <div className='icon'>
-                            <img className='icon-img' src={message.image ? message.image : profile} />
-                        </div>
-                        <div>
-                            <p >{message.user}</p>
-                            <p className='message'>{message.content}</p>
-                        </div>
-                    </li>)
-                    ) :
-                    <p>...loading</p>
-                }
-            </ul>
-            <form onSubmit={handleSubmit} className='took-form'>
-                <div className='took-form2'>
-                    <input
-                        type='text'
-                        id='tuika'
-                        placeholder='メッセージを入力'
-                        onChange={e => setValue(e.target.value)}
-                    />
-                    <div type='submit' className='took-button'></div>
-                </div>
-            </form>
-        </div>
-    </div>
-)
+    )
 }
 
 export default Room
